0bfb53d13e9013660d2232dcf2439e791f6d7e9d
reza 16t <swary2021@gmail.com>
2024-02-15 21:50:16 +0330

    adding animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,49 @@
 import { motion } from "framer-motion";
 function Home() {
+  const textVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1,
+      },
+    },
+  };
   return (
     <main className="flex h-[calc(100dvh-60px)] w-full flex-col overflow-hidden bg-[linear-gradient(180deg,#000000,#439bff40)]  md:flex-row">
-      <div className="flex h-[50%] w-full flex-col items-start gap-7 pl-[10%]  pt-5 md:h-full md:justify-center md:gap-5 md:pl-[15%] 2xl:items-start">
-        <h2 className="font-semibold tracking-[5px] text-white ">
+      <motion.div
+        variants={textVariants}
+        initial="initial"
+        animate="animate"
+        className="flex h-[50%] w-full flex-col items-start gap-7 pl-[10%]  pt-5 md:h-full md:justify-center md:gap-5 md:pl-[15%] 2xl:items-start"
+      >
+        <motion.h2
+          variants={textVariants}
+          className="font-semibold tracking-[5px] text-white "
+        >
           REZA<span className="text-primary-500 ">&</span>HAMZA
-        </h2>
-        <h1 className="flex flex-col  whitespace-nowrap text-[30px]  font-bold text-white md:text-[35px] lg:text-[40px] xl:text-[50px] 2xl:text-[70px]">
+        </motion.h2>
+        <motion.h1
+          variants={textVariants}
+          className="flex flex-col  whitespace-nowrap text-[30px]  font-bold text-white md:text-[35px] lg:text-[40px] xl:text-[50px] 2xl:text-[70px]"
+        >
           <span className="text-[33px] md:text-[36px] lg:text-[41px] xl:text-[53px] 2xl:text-[73px] ">
             Web Developers
           </span>
           <span> and Programers</span>
-        </h1>
-        <div className="flex flex-row gap-[20px]  ">
+        </motion.h1>
+
+        <motion.div
+          variants={textVariants}
+          className="flex flex-row gap-[20px]  "
+        >
           <motion.button
+            variants={textVariants}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
             className="button rounded-md bg-transparent p-2 text-[15px] font-light lg:text-[20px]"
@@ -21,19 +51,25 @@ function Home() {
             See the Latest Work
           </motion.button>
           <motion.button
+            variants={textVariants}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
             className="rounded-md bg-primary-500 p-2 text-[15px] text-white lg:text-[20px]"
           >
             Contact me
           </motion.button>
-        </div>
+        </motion.div>
         <motion.img
-          // initial={{ opacity: 1 }}
-          // animate={{ y: "5px" }}
+          // variants={textVariants}
+          className="z-[-1]"
+          animate={{
+            opacity: 0,
+            y: 10,
+            transition: { duration: 2, repeat: Infinity },
+          }}
           src="/public/scroll.png"
         ></motion.img>
-      </div>
+      </motion.div>
       {/* <div className="absolute  bottom-[-90px] z-[-2] max-w-full overflow-hidden whitespace-nowrap text-[50vh] text-[#ffffff10]">
         Writer Content Creator Influencer
       </div> */}
