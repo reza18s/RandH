302966458d64bc69bda36fedfddd3fe2ad911050
reza 16t <swary2021@gmail.com>
2024-02-16 20:15:01 +0330

    fixing the style of the parallax page

--- src/Pages/Parallax.jsx
+++ src/Pages/Parallax.jsx
@@ -15,10 +15,10 @@ function Parallax({ type }) {
   return (
     <main
       ref={ref}
-      className={`relative z-[-1] flex h-screen w-full justify-center  overflow-hidden  ${type == "services" ? "bg-[linear-gradient(180deg,#000000,#439bff40)]" : "bg-[linear-gradient(180deg,#439bff40,#000000)]"}`}
+      className={`relative z-[-1] flex h-screen w-full items-center justify-center overflow-hidden sm:items-start  ${type == "services" ? "bg-[linear-gradient(180deg,#000000,#439bff40)]" : "bg-[linear-gradient(180deg,#439bff40,#000000)]"}`}
     >
       <motion.h1
-        className="pt-[20%] text-[50px] font-semibold text-white"
+        className=" text-center text-[72px]  font-semibold text-white md:pt-[20%]"
         style={{ y: aText }}
       >
         {type == "services" ? "What We Do?" : "What we Did?"}
@@ -26,13 +26,13 @@ function Parallax({ type }) {
 
       <motion.div
         style={{ y: abg }}
-        className="absolute h-full w-full bg-[url('/planets.png')] bg-cover bg-bottom"
+        className="absolute h-full w-full bg-[url('/planets.png')] bg-contain bg-bottom bg-no-repeat md:bg-cover"
       ></motion.div>
       <motion.div
         style={{ x: abg }}
         className="absolute  h-full w-full bg-[url('/stars.png')] bg-cover bg-bottom"
       ></motion.div>
-      <motion.div className="absolute h-full w-full bg-[url('/mountains.png')] bg-cover bg-bottom"></motion.div>
+      <motion.div className="absolute h-full w-full bg-[url('/mountains.png')] bg-contain bg-bottom bg-no-repeat 2xl:bg-cover"></motion.div>
     </main>
   );
 }
